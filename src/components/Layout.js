9fd35c23909f3c73d239c38ee7d7c89ce79e5ac1
soumili-03 <soumili.biswas3183@gmail.com>
2024-07-29 23:31:19 +0530

    1st commit

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "../pages/Home";
+const Layout = () => {
+  return (
+    <>
+      <div className="sidebar-section">
+        <div className="sidebar">
+          <div className="sidebar-toggle-icons">icon</div>
+        </div>
+      </div>
+      <div className="container"></div>
+      <Home />
+    </>
+  );
+};
+
+export default Layout;
+
